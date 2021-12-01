356d55b9664efc88051076c4dcc919e1b04ef777
JoshDHoye <42089382+JoshDHoye@users.noreply.github.com>
2021-11-30 19:12:06 -0800

    First motor task upload

---
+++ src/task_motor.h
@@ -0,0 +1,11 @@
+/** @file task_motor.h
+ *  
+ * 
+ *  @author  Joshua Hoye
+ *  @date    11/30/2021
+ */
+
+void task_motor_0 (void* p_params);
+void task_motor_1 (void* p_params);
+void task_motor_2 (void* p_params);
+void task_motor_3 (void* p_params);
